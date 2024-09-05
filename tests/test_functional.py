4cd93f83e6f9c7c5192e9653ece683106bb76731
Mathieu Letrone <mathieu.letrone@gmail.com>
2024-09-05 11:56:25 +0200

    feat: add functional interfaces

---
+++ tests/test_functional.py
@@ -0,0 +1,19 @@
+from typing import Any, Callable, Dict
+
+import pytest
+
+from maybe import Supplier
+
+
+def create() -> Dict[str, str]:
+    return {"a": "test"}
+
+
+@pytest.mark.parametrize("supplier", [create, lambda: [1], list])
+def test_is_supplier(supplier: Callable[..., Any]) -> None:
+    assert isinstance(supplier, Supplier)
+
+
+@pytest.mark.parametrize("value", [{}, [1], ""])
+def test_is_not_supplier(value: Any) -> None:
+    assert not isinstance(value, Supplier)
