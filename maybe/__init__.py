61b8f09193d4a5392e4a43e37955eec54d556638
Mathieu Letrone <mathieu.letrone@gmail.com>
2024-09-05 11:58:19 +0200

    feat: add maybe container

--- maybe/__init__.py
+++ maybe/__init__.py
@@ -3,3 +3,17 @@
 from importlib.metadata import version
 
 __version__ = version(__name__)
+
+from ._exceptions import EmptyMaybeException, MaybeException
+from ._functional import Mapper, Predicate, Supplier
+from ._maybe import Empty, Maybe
+
+__all__ = [
+    "Maybe",
+    "Mapper",
+    "Supplier",
+    "Predicate",
+    "EmptyMaybeException",
+    "MaybeException",
+    "Empty",
+]
